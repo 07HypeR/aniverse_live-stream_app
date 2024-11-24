c8e0499f81c81260e3713a2fedd8637a98bd1abd
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-24 22:21:24 +0530

    AUTH SCREEN ADDED

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,18 +3,38 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Colors } from "@/utils/Constants";
 import { Stack } from "expo-router";
+import { WSProvider } from "@/service/sockets/WSProvider";
 
 const Root = () => {
   return (
     <>
-      <StatusBar
-        style="light"
-        backgroundColor={Colors.tertiary}
-        translucent={false}
-      />
-      <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-      </Stack>
+      <WSProvider>
+        <StatusBar
+          style="light"
+          backgroundColor={Colors.tertiary}
+          translucent={false}
+        />
+        <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen
+            name="auth"
+            options={{
+              animation: "fade",
+              animationDuration: 2000,
+              animationTypeForReplace: "push",
+            }}
+          />
+          <Stack.Screen
+            name="home"
+            options={{
+              animation: "fade",
+              animationDuration: 2000,
+              animationTypeForReplace: "push",
+            }}
+          />
+          <Stack.Screen name="playlist" />
+        </Stack>
+      </WSProvider>
     </>
   );
 };
